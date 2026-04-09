c667b993eab911bef892d78fba9f74ebdb6fc89d
JMartinezRuiz <75007990+JMartinezRuiz@users.noreply.github.com>
2026-04-09 04:57:46 -0600

    Update test123.py

--- test123.py
+++ test123.py
@@ -1,5 +1,1 @@
-find /productos/sproxy/SSP -type f \( -iname '*.xml' -o -iname '*.properties' -o -iname '*.conf' \) \ -exec grep -nH -E '30406|1724|HOST2_SSP_SEAS_E02_PS01|SFTP_E02_1724' {} + 2>/dev/null
-
-
-find /productos/sproxy/SSP -type f \( -iname '*.log' -o -iname '*.out' \) \ -exec grep -nH -E '30406|1724|HOST2_SSP_SEAS_E02_PS01|SFTP_E02_1724|SEAS|perimeter|listener|FAIL|ERROR|Exception|timeout|refused|handshake' {} + 2>/dev/null | tail -200
 
