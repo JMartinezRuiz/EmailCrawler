95b3325f6993d54ab1918ecf87d164454b8126e9
JMartinezRuiz <75007990+JMartinezRuiz@users.noreply.github.com>
2026-04-09 05:22:58 -0600

    Update test123.py

--- test123.py
+++ test123.py
@@ -1,9 +1,1 @@
 
-file=$(find /productos/sproxy/SSP -type f -name 'sftp.adapter-SFTP_E02_1724*.log' | sort | tail -1)
-echo "$file"
-grep -nE 'SSE1800I|SSE1831E|SSP0331E|SSP0332I|SSP0339W|SSP0340I|SSE1811E|SSE1814E' "$file" | fold -s -w 180
-
-
-find /productos/sproxy/SSP -type f -name '*.log' -print0 |
-xargs -0 grep -nE 'SSE1800I|SSE1831E|SSP0331E|SSP0332I|SSP0339W|SSP0340I|SSE1811E|SSE1814E' 2>/dev/null |
-grep 'SFTP_E02_1724\|HOST2_SEAS' | fold -s -w 180
